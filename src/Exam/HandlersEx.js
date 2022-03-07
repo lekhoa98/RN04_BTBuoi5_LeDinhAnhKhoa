ccb1e30a5acfa1ec121038fdd9701ee4520b56a9
lekhoa98 <lekhoa98.kl@gmail.com>
2022-03-07 22:12:22 +0700

    Change

--- src/Exam/HandlersEx.js
+++ src/Exam/HandlersEx.js
@@ -1,85 +1,69 @@
-import React, { Component } from "react";
-import { View, Image, TouchableOpacity, Text } from "react-native";
+import React, {Component} from 'react';
+import {View, Image, TouchableOpacity, Text} from 'react-native';
+import {Angry, Care, Haha, Like, Love, Sad} from '../emoji';
+
+const Emoji = [
+  {type: 'angry', image: Angry},
+  {type: 'care', image: Care},
+  {type: 'haha', image: Haha},
+  {type: 'like', image: Like},
+  {type: 'love', image: Love},
+  {type: 'sad', image: Sad},
+];
 
 export default class HandlersEx extends Component {
-    state = {
-        imageDefault: require('../emoji/angry.png'),
-    }
-
-    onPressAngry = () => {
-
-        this.setState({ imageDefault: require('../emoji/angry.png') })
-    }
-    onPressCare = () => {
-
-        this.setState({ imageDefault: require('../emoji/care.png') })
-    }
-    onPressHaha = () => {
-
-        this.setState({ imageDefault: require('../emoji/haha.png') })
-    }
-    onPressLike = () => {
-
-        this.setState({ imageDefault: require('../emoji/like.png') })
-    }
-    onPressLove = () => {
-
-        this.setState({ imageDefault: require('../emoji/love.png') })
-    }
-    onPressSad = () => {
-
-        this.setState({ imageDefault: require('../emoji/sad.png') })
-    }
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold' }}>Bạn cảm thấy như thế nào?</Text>
-                </View>
-                <View style={styles.box1}>
-                    <Image source={this.state.imageDefault} />
-                </View>
-                <View style={styles.box2}>
-                    <TouchableOpacity style={styles.btn} onPress={this.onPressAngry}>
-                        <Image style={styles.icon} source={require('../emoji/angry.png')} />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.btn} onPress={this.onPressCare}>
-                        <Image style={styles.icon} source={require('../emoji/care.png')} />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.btn} onPress={this.onPressHaha}>
-                        <Image style={styles.icon} source={require('../emoji/haha.png')} />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.btn} onPress={this.onPressLike}>
-                        <Image style={styles.icon} source={require('../emoji/like.png')} />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.btn} onPress={this.onPressLove}>
-                        <Image style={styles.icon} source={require('../emoji/love.png')} />
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.btn} onPress={this.onPressSad}>
-                        <Image style={styles.icon} source={require('../emoji/sad.png')} />
-                    </TouchableOpacity>
-                </View>
-            </View>
-        )
-    }
+  state = {
+    imageDefault: Emoji[0].image,
+    imageOption: Emoji,
+  };
+  renderEmoji = () => {
+    return this.state.imageOption.map((item,index) => {
+      return (
+        <TouchableOpacity
+        key={index}
+          style={styles.btn}
+          onPress={() => {
+            console.log(item.type);
+            this.setState({imageDefault: item.image});
+          }}>
+          <Image style={styles.icon} source={item.image} />
+        </TouchableOpacity>
+      );
+    });
+  };
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={{textAlign: 'center', fontSize: 20, fontWeight: 'bold'}}>
+            Bạn cảm thấy như thế nào?
+          </Text>
+        </View>
+        <View style={styles.box1}>
+          <Image source={this.state.imageDefault} />
+        </View>
+        <View style={styles.box2}>{this.renderEmoji()}</View>
+      </View>
+    );
+  }
 }
 const styles = {
-    box1: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    box2: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    icon: {
-        height: 50,
-        width: 50,
-    },
-    btn: {
-        margin: 5,
-    }
-}
+  box1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box2: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    height: 50,
+    width: 50,
+  },
+  btn: {
+    margin: 5,
+  },
+};
---
+++ src/emoji/index.js
@@ -0,0 +1,8 @@
+import Angry from './angry.png';
+import Care from './care.png';
+import Haha from './haha.png';
+import Like from './like.png';
+import Love from './love.png';
+import Sad from './sad.png';
+
+export {Angry, Care, Haha, Like, Love, Sad};
